94a45d5d5b1081590d83c5907a98d1285c6a288d
sachin-create <52964301+sachin-create@users.noreply.github.com>
2020-03-19 08:20:05 +0530

    Delete file1.c

--- file1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#define MAX(a,b) a>b?a:b
-#define MAXX(a,b,c) c>MAX(a,b)?c:MAX(a,b)
-int main()
-{
-    int a=10,b=3,c=60;
-    printf("%d\n",MAX(a,b));
-    printf("%d",MAXX(a,b,c));
-}
